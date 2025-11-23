ed780f1d31580381a0a0b3ac025bc98de50b0dab
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-11-23 23:21:35 +0530

    added files for all pages in src folder and installed react-router-DOM package and added paths to all pages in main.jsx file

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import login from './pages/login'
+import feed from './pages/feed'
+import messages from './pages/messages'
+import chatbox from './pages/chatbox'
+import connections from './pages/connections'
+import discover from './pages/discover'
+import profile from './pages/profile'
+import createpost from './pages/createpost'
 
 const app = () => {
   return (
-    <div>app</div>
+    <>
+     <Routes>
+        <Route path ='/' element ={<login/>}>
+          <Route index element ={<feed/>}/>
+          <Route path='messages' element ={<messages/>}/>
+          <Route path='messages/:userid' element ={<chatbox/>}/>
+          <Route path='connections' element ={<connections/>}/>
+          <Route path='discover' element ={<discover/>}/>
+          <Route path='profile' element ={<profile/>}/>
+          <Route path='profile/:profileid' element ={<profile/>}/>
+          <Route path='createpost' element ={<createpost/>}/>
+        </Route>
+     </Routes>
+    </>
   )
 }
 
