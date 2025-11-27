5c539a6cbc29c170b4a821a5adf7ec69868f29fb
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-11-28 03:38:11 +0530

    Completed Discover page with all the buttons

--- client/src/pages/connections.jsx
+++ client/src/pages/connections.jsx
@@ -66,7 +66,7 @@ const Connections = () => {
                   <p className="text-sm text-gray-600">{user.bio.slice(0, 30)}...</p>
                   <div className='flex max-sm:flex-col gap-2 mt-4'>
                     {
-                      <button onClick={()=> navigate(`/profile/${user._id}`)} className='w-full p-2 text-sm rounded bg-gradient-to-r from-orange-500 to-red-600 hover:from-orange-600 to-red-700 active:scale-95 transition text-white cursor pointer'>
+                      <button onClick={()=> navigate(`/profile/${user._id}`)} className='w-full p-2 text-sm rounded bg-linear-to-r from-orange-500 to-red-600 hover:from-orange-600 hover:to-red-700 active:scale-95 transition text-white cursor pointer'>
                         View Profile
                       </button>
                     }
