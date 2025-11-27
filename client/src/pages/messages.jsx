2fc96a7c561146ba1dd4f9d5aa615e983dbead8a
Vedant-Jambhekar <dypu.bca.vedantjambhekar@gmail.com>
2025-11-27 18:07:21 +0530

    updated Messages buttons

--- client/src/pages/messages.jsx
+++ client/src/pages/messages.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
+import { Eye, MessageSquare } from 'lucide-react'
 
 const messages = () => {
+
+  const navigate = useNa
+
   return (
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
@@ -12,14 +16,24 @@ const messages = () => {
         </div>
 
         {/* Connected Users */}
-        <div>
+        <div className='flex flex-col gap-3'>
           {dummyConnectionsData.map((user)=>(
-            <div key={user._id} className='max-w-xl flex flex-wrap gap-6 p-6 bg-white shadow rounded-md'>
+            <div key={user._id} className='max-w-xl flex flex-warp gap-6 p-6 bg-white shadow rounded-md'>
               <img src={user.profile_picture} alt="" className='rounded-full size-12 mx-auto'/>
-              <div>
-                <p>{user.full_name}</p>
-                <p>@{user.username}</p>
-                <p>{user.bio}</p>
+              <div className='flex-1'>
+                <p className='font-medium text-slate-700'>{user.full_name}</p>
+                <p className='text-slate-500'>@{user.username}</p>
+                <p className='text-sm text-gray-600'>{user.bio}</p>
+              </div>
+
+              <div className='flex flex-col gap-2 mt-4'>
+                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
+                  <MessageSquare className='w-4 h-4'/>
+                </button>
+
+                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+                  <Eye className='w-4 h-4'/>
+                </button>
               </div>
             </div>
           ))}
