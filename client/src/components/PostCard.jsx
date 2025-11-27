cd3ab1665495a721d9e6d2d4b3e004ef4408ada0
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-11-27 22:43:15 +0530

    Added Navigation to chat and profile from messages section; Added navigation to profile in feed section

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,19 +2,23 @@ import React, { useState } from 'react'
 import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
 import moment from 'moment'
 import { dummyUserData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({post}) => {
 
     const postwithHashtags = post.content.replace(/(#\w+)/g, '<span class="text-blue-800">$1</span>')
     const[likes, setLikes] = useState(post.likes_count)
     const currentUser = dummyUserData
+
     const handleLike = async () => {
 
     }
+
+    const navigate = useNavigate()
   return (
     <div className='bg-white rounded-xl shadow p-4 space y-4 w-full max-w-2xl'>
         {/* user info */}
-        <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <div onClick={()=> navigate(`/profile/` + post.user._id)} className='inline-flex items-center gap-3 cursor-pointer'>
             <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow'/>
             <div>
                 <div className='flex items-center space-x-1'>
@@ -53,7 +57,7 @@ const PostCard = ({post}) => {
             <Share2 className='w-4 h-4'/>            
             <span>{7}</span>
         </div>
-
+           
        </div>
 
 
--- client/src/pages/messages.jsx
+++ client/src/pages/messages.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
 import { Eye, MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const messages = () => {
 
-  const navigate = useNa
+  const navigate = useNavigate()
 
   return (
     <div className='min-h-screen relative bg-slate-50'>
@@ -27,11 +28,11 @@ const messages = () => {
               </div>
 
               <div className='flex flex-col gap-2 mt-4'>
-                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
+                <button onClick={()=> navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
                   <MessageSquare className='w-4 h-4'/>
                 </button>
 
-                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+                <button onClick={()=> navigate(`/profile/${user._id}`)}className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
                   <Eye className='w-4 h-4'/>
                 </button>
               </div>
