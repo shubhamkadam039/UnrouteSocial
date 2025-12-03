691d7dede93478f25b5a55be284367cd7b0a3cf9
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-12-03 17:39:55 +0530

    Revert "Delete client directory"
    This reverts commit 68215a661cb8d50b7b29b3cf65bbfec2d4c1aaf4.

---
+++ client/src/app.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react'
+
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import Messages from './pages/Messages'
+import Connections from './pages/Connections'
+import Profile from './pages/Profile'
+import Feed from './pages/Feed'
+import Discover from './pages/Discover'
+import CreatePost from './pages/Createpost'
+import Chatbox from './pages/ChatBox'
+
+
+const app = () => {
+  const {user} = useUser()
+  return (
+    <>
+     <Toaster />
+     <Routes>
+        <Route path ='/' element ={ !user ? <Login/> : <Layout />}>
+          <Route index element ={<Feed/>}/>
+          <Route path='Messages' element ={<Messages/>}/>
+          <Route path='Messages/:userid' element ={<Chatbox/>}/>
+          <Route path='Connections' element ={<Connections/>}/>
+          <Route path='Discover' element ={<Discover/>}/>
+          <Route path='Profile' element ={<Profile/>}/>
+          <Route path='Profile/:Profileid' element ={<Profile/>}/>
+          <Route path='Createpost' element ={<CreatePost/>}/>
+        </Route>
+     </Routes>
+    </>
+  )
+}
+
+export default app
