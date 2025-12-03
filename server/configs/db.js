2ef1e064a8d1700ad89457b4775c7572908ee4bc
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-12-03 16:08:22 +0530

    Fixed flow of connecting database

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,15 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from "mongoose";
 
-console.log(process.env.MONGODB_URL)
-
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}/UnrouteSocial`)
         console.log('MongoDB connected successfully')
     } catch (error) {
         console.log(error.message)
-        
+        process.exit(1)
     }
     
 }
--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 import express from 'express';
-import { serve } from 'inngest/express';    
+// import { serve } from 'inngest/express';    
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './configs/db.js';
-import {inngest, functions} from './inngest/index.js'; 
+// import {inngest, functions} from './inngest/index.js'; 
 const app = express();
 
-await connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res)=> res.send('Server is running'))
-app.use('/api/inngest', serve({ client: inngest, functions }));
+// app.use('/api/inngest', serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+})
