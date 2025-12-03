af35fad09234395c42305e68e2e40354e51f8b83
shubhamkadam039 <bca.shubhamkadam@gmail.com>
2025-12-03 15:57:08 +0530

    Added database connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import mongoose from "mongoose";
+
+console.log(process.env.MONGODB_URL)
 
 const connectDB = async () => {
     try {
-        mongoose.connection.on('connected', ()=> console.log('Database connected'))
         await mongoose.connect(`${process.env.MONGODB_URL}/UnrouteSocial`)
+        console.log('MongoDB connected successfully')
     } catch (error) {
         console.log(error.message)
         
